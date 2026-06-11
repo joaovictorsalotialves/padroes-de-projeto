d40bb672fa06733de2575c104b8be7acbb193339
João Victor <joaovictorsalotialves12@gmail.com>
2026-06-11 14:31:19 -0300

    Implementando log no banco

--- src/adapters/decorator/logErrorControllerDecorator.ts
+++ src/adapters/decorator/logErrorControllerDecorator.ts
@@ -1,13 +1,17 @@
+import type { LogErrorRepository } from '../../usecases/repository/logErrorRepository'
 import type { Controller } from '../interfaces/controller'
 import type { HttpRequest, HttpResponse } from '../interfaces/http'
 
 export class LogErrorControllerDecorator implements Controller {
-  constructor(private readonly controller: Controller) {}
+  constructor(
+    private readonly controller: Controller,
+    private readonly logErrorRepository: LogErrorRepository
+  ) {}
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     const httpResponse = await this.controller.handle(httpRequest)
 
     if (httpResponse.statusCode === 500) {
-      console.log('Log do erro')
+      await this.logErrorRepository.log(httpResponse.body.stack)
     }
 
     return httpResponse
--- src/adapters/factories/taskControllerFactory.ts
+++ src/adapters/factories/taskControllerFactory.ts
@@ -1,4 +1,5 @@
 import { DbAddTask } from '../../dataSources/db/dbAddTask'
+import { LogErrorMongoRepository } from '../../dataSources/db/repository/logErrorMongoRepository'
 import { TaskMongoRepository } from '../../dataSources/db/repository/taskMongoRepository'
 import { AddTaskController } from '../controllers/task/addTask'
 import { DateValidatorAdapter } from '../dateValidadeAdapter'
@@ -9,5 +10,6 @@ export const taskControllerFactory = () => {
   const dbAddTask = new DbAddTask(taskMongoRepository)
   const dateValidadeAdapter = new DateValidatorAdapter()
   const taskController = new AddTaskController(dbAddTask, dateValidadeAdapter)
-  return new LogErrorControllerDecorator(taskController)
+  const logErrorMongoRepository = new LogErrorMongoRepository()
+  return new LogErrorControllerDecorator(taskController, logErrorMongoRepository)
 }
