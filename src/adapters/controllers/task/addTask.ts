258d53db9dc8d40bdf039e251e4ca1e1209f0ab6
João Victor <joaovictorsalotialves12@gmail.com>
2026-06-11 16:49:48 -0300

    Testando o agrupador de validações

--- src/adapters/controllers/task/addTask.ts
+++ src/adapters/controllers/task/addTask.ts
@@ -1,7 +1,7 @@
 import type { AddTask } from '../../../usecases/addTask'
 import type { Controller } from '../../interfaces/controller'
-import type { DateValidator } from '../../interfaces/dateValidator'
 import type { HttpRequest, HttpResponse } from '../../interfaces/http'
+import type { Validation } from '../../interfaces/validation'
 import { InvalidParamError } from '../../presentations/api/errors/invalid-param-error'
 import { MissingParamError } from '../../presentations/api/errors/missing-param-error'
 import { badRequest, created, serverError } from '../../presentations/api/httpResponses/httpResponses'
@@ -9,25 +9,17 @@ import { badRequest, created, serverError } from '../../presentations/api/httpRe
 export class AddTaskController implements Controller {
   constructor(
     private readonly addTask: AddTask,
-    private readonly dateValidator: DateValidator
+    private readonly validation: Validation
   ) {}
 
   async handle(httpRequest: HttpRequest): Promise<HttpResponse> {
     try {
-      const requiredFields = ['title', 'description', 'date']
-
-      for (const field of requiredFields) {
-        if (!httpRequest.body[field]) {
-          return badRequest(new MissingParamError(field))
-        }
+      const error = this.validation.validate(httpRequest.body)
+      if (error) {
+        return badRequest(error)
       }
+
       const { title, description, date } = httpRequest.body
-
-      const isValid = this.dateValidator.isValid(date)
-
-      if (!isValid) {
-        return badRequest(new InvalidParamError('date'))
-      }
 
       const task = await this.addTask.add({ title, description, date })
       return created(task)
--- src/adapters/factories/taskControllerFactory.ts
+++ src/adapters/factories/taskControllerFactory.ts
@@ -2,14 +2,13 @@ import { DbAddTask } from '../../dataSources/db/dbAddTask'
 import { LogErrorMongoRepository } from '../../dataSources/db/repository/logErrorMongoRepository'
 import { TaskMongoRepository } from '../../dataSources/db/repository/taskMongoRepository'
 import { AddTaskController } from '../controllers/task/addTask'
-import { DateValidatorAdapter } from '../dateValidadeAdapter'
 import { LogErrorControllerDecorator } from '../decorator/logErrorControllerDecorator'
+import { addTaskValidationCompositeFactory } from './addTaskValidationCompositeFactory'
 
 export const taskControllerFactory = () => {
   const taskMongoRepository = new TaskMongoRepository()
   const dbAddTask = new DbAddTask(taskMongoRepository)
-  const dateValidadeAdapter = new DateValidatorAdapter()
-  const taskController = new AddTaskController(dbAddTask, dateValidadeAdapter)
+  const taskController = new AddTaskController(dbAddTask, addTaskValidationCompositeFactory())
   const logErrorMongoRepository = new LogErrorMongoRepository()
   return new LogErrorControllerDecorator(taskController, logErrorMongoRepository)
 }
